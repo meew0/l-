b82f6eb0b01d8d9201760a4ec2aabfd4034535ae
meew0 <blactbt@live.de>
2016-02-10 21:51:20 +0100

    Make a fail_invalid helper that creates a Discord format response

--- rest/helpers.rb
+++ rest/helpers.rb
@@ -2,12 +2,16 @@ require 'json'
 
 module Ldash
   module RestHelpers
+    def fail_invalid(ldash_message, status = 400, discord_message = '')
+      halt status, %Q({"message": "#{discord_message}", "l-": "#{ldash_message}"})
+    end
+
     def json!
       request.body.rewind
       JSON.parse(request.body.read)
     rescue
       # Invalid JSON
-      halt 400, '{"message": ""}'
+      fail_invalid 'invalid JSON'
     end
   end
 end
