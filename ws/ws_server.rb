fab165675124505a8f61b1b5290f9344c2e3a488
meew0 <blactbt@live.de>
2016-02-19 20:04:36 +0100

    Do session checking in the WS open event

--- ws/ws_server.rb
+++ ws/ws_server.rb
@@ -1,6 +1,8 @@
 require 'eventmachine'
 require 'faye/websocket'
 
+require 'ws/ws_wrapper'
+
 Faye::WebSocket.load_adapter('thin')
 
 module Ldash
@@ -15,6 +17,19 @@ module Ldash
 
         ws.on :open do |event|
           puts "Incoming WS connection - object ID: #{event.object_id}"
+
+          if defined? $session
+            if $session.ws?
+              puts "Already connected! Closing WS connection for #{event.object_id}"
+              ws.close(4002, 'l-: Already connected')
+            else
+              $session.ws = WSWrapper.new(ws)
+              puts "WS connection for #{event.object_id} succeeded"
+            end
+          else
+            puts "No session! Closing WS connection for #{event.object_id}"
+            ws.close(4001, 'l-: No session defined')
+          end
         end
 
         ws.on :message do |event|
