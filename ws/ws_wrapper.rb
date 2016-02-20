2200876e83f6e4da119037bb6354ec8cfd50a189
meew0 <blactbt@live.de>
2016-02-20 11:40:33 +0100

    Implement basic WS sending functionality

--- ws/ws_wrapper.rb
+++ ws/ws_wrapper.rb
@@ -1,16 +1,30 @@
+require 'json'
+
 module Ldash
   # Class that wraps around Faye's ws object so we can send events more easily
   class WSWrapper
     def initialize(ws)
       @ws = ws
+      @sequence = 0
     end
 
     def dummy?
       false
     end
 
-    def send_ready(session)
+    def ready!
       # ...
+    end
+
+    def packet(type, data)
+      packet = {
+          t: type,
+          s: (@sequence += 1),
+          op: 0,
+          d: data
+      }
+
+      @ws.send(packet.to_json)
     end
   end
 end
