d919b47532d3bbd72c8c44a972b59557c5019b87
meew0 <blactbt@live.de>
2016-02-20 15:15:35 +0100

    Implement bot_user and session_id in the model

--- common/model.rb
+++ common/model.rb
@@ -13,6 +13,8 @@ module Ldash
 
     attr_accessor :email, :password
 
+    attr_accessor :verified
+
     def initialize(data)
       @username = data['username'].to_s
       @id = data['id'].to_i
@@ -22,23 +24,50 @@ module Ldash
       @avatar = data['avatar'].to_s if data['avatar']
       @email = data['email'].to_s if data['email']
       @password = data['password'].to_s if data['password']
+
+      @verified = data['verified'].nil? ? true : data['verified']
     end
 
     def compact
       {
-        username: @username.to_s,
-        id: @id.to_s,
-        discriminator: @discriminator.to_s,
-        avatar: @avatar.to_s
+          username: @username.to_s,
+          id: @id.to_s,
+          discriminator: @discriminator.to_s,
+          avatar: @avatar.to_s
       }
     end
+
+    # The email will only ever be defined on a possible bot user
+    def bot_user?
+      !@email.nil?
+    end
+
+    def bot_user
+      compact.merge({
+          verified: @verified,
+          email: @email.to_s
+                    })
+    end
   end
+
+  # The default user in case l- needs one but none exists
+  DEFAULT_USER = User.new({
+      id: 66237334693085184,
+      username: 'meew0',
+      avatar: 'd18a450706c3b6c379f7e2329f64c9e7',
+      discriminator: 3569,
+      email: 'meew0@example.com',
+      password: 'hunter2',
+      verified: true
+                          })
 
   # L- session
   class Session
     attr_accessor :users, :channels, :servers, :messages, :roles, :tokens
     attr_accessor :ws
     attr_accessor :large_threshold
+
+    attr_reader :id
 
     def initialize
       @users = []
@@ -51,6 +80,7 @@ module Ldash
       @token_num = 0
 
       @ws = DummyWS.new
+      @id = self.object_id.to_s(16).rjust(32, '0')
     end
 
     def ws?
@@ -75,6 +105,13 @@ module Ldash
 
     def token?(token)
       @tokens.include? token
+    end
+
+    def bot_user
+      user = @users.find(&:bot_user)
+
+      # If none exists, use the default user
+      user || DEFAULT_USER
     end
   end
 end
