b36f3518c5d52f264a12005aaa985660df6a11bd
meew0 <blactbt@live.de>
2016-03-25 16:14:14 +0100

    Slightly restructure the default preset

--- presets/default.rb
+++ presets/default.rb
@@ -1,23 +1,25 @@
 # This file is the default session that l- loads if no other session preset is specified
 
-bot = Ldash::User.new(
-  $session,
+bot = User.new(
+  self,
   name: 'l- bot',
   email: 'abc@test.com',
   password: 'l- is awesome!',
   verified: true
 )
 
-server = Ldash::Server.new(
-  $session,
+server = Server.new(
+  self,
   name: 'l- test server',
   owner_id: bot.id
 )
 
-bot_member = Ldash::Member.new(
-  $session,
+bot_member = Member.new(
+  self,
   user: bot,
   server: server
 )
 
 server.members << bot_member
+@users << bot
+@servers << server
